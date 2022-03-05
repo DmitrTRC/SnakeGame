925505a8b794368d976c54cb15e456b08efc6176
Dmitry Morozov <9984398@gmail.com>
2022-03-05 22:49:03 +0300

    Initial commit

---
+++ include/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Dmitry Morozov on 05.03.2022.
+//
+
+#ifndef SNAKEGAME_MAIN_H
+#define SNAKEGAME_MAIN_H
+
+#endif //SNAKEGAME_MAIN_H
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Dmitry Morozov on 05.03.2022.
+//
+
+#include "../include/main.h"
+#include <iostream>
+
+
+int main () {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
