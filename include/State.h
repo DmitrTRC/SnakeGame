038b2b1be8164873645b9a075af62d22118e0415
Dmitry Morozov <9984398@gmail.com>
2022-03-27 15:18:41 +0300

    feat(State header implemented): State class draft implemented.

--- include/State.h
+++ include/State.h
@@ -5,9 +5,34 @@
 #ifndef SNAKEGAME_STATE_H
 #define SNAKEGAME_STATE_H
 
+#include <SFML/Graphics.hpp>
+
 
 class State {
+public:
+    virtual bool switchState ();
 
+    virtual void updateView (double dt) = 0;
+
+    virtual void draw (sf::RenderWindow) = 0;
+
+    virtual void processEvent (double dt, sf::Vector2f mousePos) = 0;
+
+    virtual void handleInput (double dt) = 0;
+
+    virtual void onQuit ();
+
+    bool getQuitState (bool quit);
+
+    void setQuitState (bool quit);
+
+    void setScoreHolder (int score);
+
+    int getScoreHolder ();
+
+private:
+    bool quitState;
+    int scoreHolder;
 };
 
 
